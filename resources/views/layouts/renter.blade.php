e0cccab667aea5556c6a7bb41b073e8960a595bc
Bipin Paudel <bipo_bca2076@lict.edu.np>
2023-07-30 14:31:11 +0545

    Added documentation file

--- resources/views/layouts/renter.blade.php
+++ resources/views/layouts/renter.blade.php
@@ -19,6 +19,7 @@
 
     <script src="https://khalti.s3.ap-south-1.amazonaws.com/KPG/dist/2020.12.17.0.0.0/khalti-checkout.iffe.js"></script>
 
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles()
 </head>
