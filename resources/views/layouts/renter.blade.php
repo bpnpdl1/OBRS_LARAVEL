271635e7fdc086d7acffc743ae93ce00140491bd
Bipin Paudel <bipo_bca2076@lict.edu.np>
2024-09-13 23:22:24 +0545

    changed vite code

--- resources/views/layouts/renter.blade.php
+++ resources/views/layouts/renter.blade.php
@@ -20,7 +20,13 @@
     <script src="https://khalti.s3.ap-south-1.amazonaws.com/KPG/dist/2020.12.17.0.0.0/khalti-checkout.iffe.js"></script>
 
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if (env('APP_ENV') === 'local')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        {{-- Tailwind cdn --}}
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2"></script>
+    @endif
     @livewireStyles()
 </head>
 
